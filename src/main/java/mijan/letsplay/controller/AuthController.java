f34560acfc3b31c71bea9bbbf7662455cff6917b
terujd <jansson.michael.93@gmail.com>
2023-10-29 00:55:00 +0300

    added comments

--- src/main/java/mijan/letsplay/controller/AuthController.java
+++ src/main/java/mijan/letsplay/controller/AuthController.java
@@ -17,36 +17,41 @@ import mijan.letsplay.repositories.UserRepository;
 import mijan.letsplay.services.JWTService;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/api/auth")//the base url for the auth controller
+// AuthController is a controller class that will be used to handle requests to the /api/auth endpoint.
 public class AuthController {
 
+    // The JWTService bean is used to generate and validate the JWT
     @Autowired
     private JWTService jwtService;
 
+    // The UserRepository bean is used to perform CRUD operations on the User collection.
     @Autowired
     private UserRepository userRepository;
 
+    // The PasswordEncoder bean is used to encode the password
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    // The authenticateAndGetToken method is used to authenticate the user and generate the JWT.
     @PostMapping
     public ResponseEntity<?> authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
         try {
             // Basic input validation to prevent injection attacks
-            if (authRequest.getUsername() == null || authRequest.getPassword() == null) {
-                return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);
+            if (authRequest.getUsername() == null || authRequest.getPassword() == null) {//check if the username and password are null
+                return new ResponseEntity<>("Invalid input", HttpStatus.BAD_REQUEST);//return a bad request message
             }
 
-            Optional<User> user = userRepository.findByName(authRequest.getUsername());
-            if (user.isPresent()) {
-                if (passwordEncoder.matches(authRequest.getPassword(), user.get().getPassword())) {
-                    String token = jwtService.generateToken(user.get().getName());
-                    return new ResponseEntity<>(token, HttpStatus.OK);
+            Optional<User> user = userRepository.findByName(authRequest.getUsername());//find the user by username
+            if (user.isPresent()) {//check if the user is present
+                if (passwordEncoder.matches(authRequest.getPassword(), user.get().getPassword())) {//check if the password matches
+                    String token = jwtService.generateToken(user.get().getName());//generate the token
+                    return new ResponseEntity<>(token, HttpStatus.OK);//return a success message
                 } else {
-                    return new ResponseEntity<>("Wrong password", HttpStatus.UNAUTHORIZED);
+                    return new ResponseEntity<>("Wrong password", HttpStatus.UNAUTHORIZED);//return an unauthorized message
                 }
             } else {
-                return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);//return a not found message
             }
         } catch (Exception e) {
             // Generic catch block to ensure no 5XX errors are returned
